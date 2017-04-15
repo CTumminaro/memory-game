bec67d68a2f3ba18dcbb4e6cbd218cf7fb6fd7c4
Christopher <CTumminaro@gmail.com>
2017-04-15 17:19:42 -0500

    adding in more characters

--- src/sample-data.js
+++ src/sample-data.js
@@ -28,5 +28,35 @@ module.exports = [
     id: 5,
     name: "Susie Sheep",
     img: '/images/peppa-pig/susie-sheep.jpg'
+  },
+  {
+    id: 6,
+    name: "Miss Rabbit",
+    img: '/images/peppa-pig/miss-rabbit.jpg'
+  },
+  {
+    id: 7,
+    name: "Madame Gazelle",
+    img: '/images/peppa-pig/madame-gazelle.jpg'
+  },
+  {
+    id: 8,
+    name: "Danny Dog",
+    img: '/images/peppa-pig/danny-dog.jpg'
+  },
+  {
+    id: 9,
+    name: "Zoe Zebra",
+    img: '/images/peppa-pig/zoe-zebra.jpg'
+  },
+  {
+    id: 10,
+    name: "Grandpa Pig",
+    img: '/images/peppa-pig/grandpa-pig.jpg'
+  },
+  {
+    id: 11,
+    name: "Granny Pig",
+    img: '/images/peppa-pig/granny-pig.jpg'
   }
 ]
